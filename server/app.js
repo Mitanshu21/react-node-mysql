51b2baf3cafac5e7f5a5be5b241ec58533989608
mitanshu patel <mitanshu.patel@bacancy.com>
2022-12-31 13:33:17 +0530

    initial setup

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const db = require("./database");
+const PORT = 5000;
+
+db.connect((err) => {
+  if (err) {
+    console.error("error connecting: " + err);
+    return;
+  }
+  console.log("Connection Successful !!");
+});
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+
+app.get("/", (req, res) => {
+  console.log("home");
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("Listning at Port no.", PORT);
+});
